80b8f32ba4d0f99f228c343ef70ced9f06648590
DaniAm94 <dani.amico94@gmail.com>
2023-12-15 15:38:31 +0100

    vars recovery from user

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 console.log('checking js file link')
+
+const firstName = prompt('Scrivi il tuo nome: ')
+console.log(firstName);
+const lastName = prompt('Scrivi il tuo cognome: ')
+console.log(lastName);
+const favoriteColor = prompt('Scrivi il tuo colore preferito: ')
+console.log(favoriteColor);
