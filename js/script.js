8ffba30e139937883e0133896899899dc0e4615f
DaniAm94 <dani.amico94@gmail.com>
2023-12-15 16:09:01 +0100

    print the message on the viewport

--- js/script.js
+++ js/script.js
@@ -7,7 +7,23 @@ console.log(lastName);
 const favoriteColor = prompt('Scrivi il tuo colore preferito: ')
 console.log(favoriteColor);
 
+/* --------Bonus------- */
+// Prendo l'elemento HTML
+const messageOnScreen = document.getElementById('iShowTheMessage')
+/*--------------------- */
+
 const password = firstName + lastName + favoriteColor + 21
 console.log(password);
 
 const message = `La tua password è: ${password}`;
+console.log(message);
+
+/*------- Bonus -------- */
+// Stampo il messaggio nell'elemento HTML
+
+// Opzione 1 (Commentare per usare la 2)
+messageOnScreen.innerText = message;
+
+//Opzione 2 (Decommentare)
+// messageOnScreen.innerHTML = `La tua password è: <strong>${password}</strong>`
+/*----------------------- */
