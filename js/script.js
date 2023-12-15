df32a6f7dc994b2295ec6a8d2fe7e9ba0753797b
DaniAm94 <dani.amico94@gmail.com>
2023-12-15 15:40:08 +0100

    password generation

--- js/script.js
+++ js/script.js
@@ -6,3 +6,6 @@ const lastName = prompt('Scrivi il tuo cognome: ')
 console.log(lastName);
 const favoriteColor = prompt('Scrivi il tuo colore preferito: ')
 console.log(favoriteColor);
+
+const password = firstName + lastName + favoriteColor + 21
+console.log(password);
